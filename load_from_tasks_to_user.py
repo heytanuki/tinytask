2e28a7acc4ad998b94154b175806d1976564b4c9
heytanuki <7634261+heytanuki@users.noreply.github.com>
2017-09-25 10:52:48 -0500

    removing temp loader script

--- load_from_tasks_to_user.py
+++
@@ -1,9 +0,0 @@
-from tasklist import TaskDB
-db = TaskDB()
-
-username = 'heytanuki@gmailcom'
-
-my_data = db.database.child('tasks').get(db.get_token()).val()
-
-for item in my_data:
-    db.database.child(username).child('tasks').child(item).set(my_data[item], db.get_token())

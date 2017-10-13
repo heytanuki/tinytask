dc1533ebd986231209c057359697ef8684842032
tanuki <7634261+heytanuki@users.noreply.github.com>
2017-10-13 13:10:48 -0500

    auth flow: checking against existing email address

--- main.py
+++ main.py
@@ -129,6 +129,9 @@ def get_stats_on(task_list):
 def check_user_initialization(username, email):
     user_db = UserTasks(username, task_db)
     settings = user_db.get_user_settings()
+    if 'email' in settings:
+        if email != settings['email']:
+            return flask.redirect(flask.url_for('not_authorized'))
     if settings is None or 'email' not in settings:
         user_db.set_user_setting('email', email)
     if settings is None or 'secret' not in settings:
